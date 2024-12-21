c6dabf3f3ac75cb7193605247cebae23eff0f5a2
Nesty Omongos <HZ202301549@wmsu.edu.ph>
2024-12-22 00:08:40 +0800

    Add files via upload

---
+++ entry.php
@@ -0,0 +1,14 @@
+<?php
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+
+$uri .= $_SERVER['HTTP_HOST'];
+
+$uri .= $_SERVER["REQUEST_URI"];
+
+header('Location: ' . $uri . "sms/main/auth/login.php");
+
+exit;
